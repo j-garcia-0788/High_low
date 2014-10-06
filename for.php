2634253a1aab76bfcc338a35e78411b98a31c2f2
James Garcia <james.m.garcia7@gmail.com>
2014-10-06 17:14:09 -0500

    for

--- for.php
+++ for.php
@@ -8,8 +8,16 @@ fwrite(STDOUT, "Enter a minimum a number. \n");
 fwrite(STDOUT, "Enter an maximum number. \n");
 	$ending = trim(fgets(STDIN));
 
-fwrite(STDOUT, 'Enter incrementation. ');
+if (!is_numeric($starting) || !is_numeric($ending)){
+	fwrite(STDOUT, "Hey! We need a number! \n");
+	exit(0);
+}
+
+fwrite(STDOUT, "Enter incrementation. \n");
 	$count = trim(fgets(STDIN));
+if (!is_numeric($count)){
+		$count = 1;
+}
 
 for ($i = $starting; $i <= $ending; $i += $count) {
     echo "\$i has a value of {$i}\n";
