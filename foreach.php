9d735fd603ab11549ed98e825c1e6804195f7f17
James Garcia <james.m.garcia7@gmail.com>
2014-10-07 09:46:25 -0500

    for each

---
+++ foreach.php
@@ -0,0 +1,21 @@
+<?php
+
+$things = array('Sgt. Pepper', "11", null, array(1,2,3), 3.14, "12 + 7", false, (string) 11);
+
+foreach ($things as $thing) {
+    if (is_int($thing)) {
+        echo "{$thing} is a number\n";
+    } else if (is_float($thing)) {
+        echo "{$thing} is a float\n";
+    } else if (is_bool($thing)) {
+        echo "\n";
+    } else if (is_array($thing)) {
+        echo "{$thing} is an array\n";
+    } else if (is_null($thing)) {
+        echo "\n";
+    } else if (is_string($thing)) {
+        echo "{$thing} is a string\n";
+    } else if (is_scalar($thing)) {
+        echo "{$thing} is a float, integer, string, or boolean\n";
+    }
+}
