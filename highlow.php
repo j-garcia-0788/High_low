fd12c699aeb22d86fb126caee744eb88a92c19a5
James Garcia <james.m.garcia7@gmail.com>
2014-10-07 09:06:50 -0500

    highlow edit

--- highlow.php
+++ highlow.php
@@ -1,25 +1,24 @@
 <?php
 
-// Write the output
-// Notice the space after the ?
 fwrite(STDOUT, 'Guess the number between 1 and 100 ');
 
-// Get the input from user
 $answer = trim(fgets(STDIN));
 $rightanswer = (rand(0 , 100));
+$count = 1;
 
-// Output the user's name
-
-	while ($answer < $rightanswer || $answer > $rightanswer || $answer == $rightanswer){
-		if ($answer < $rightanswer) {
-			fwrite(STDOUT, "Guess higher!" . "\n");
-			$answer = trim(fgets(STDIN));
+while ($answer != $rightanswer){
+	
+	if ($answer < $rightanswer) {
+		fwrite(STDOUT, "Guess higher!" . "\n");
+	
+	} elseif($answer > $rightanswer) {
+		fwrite(STDOUT, "Guess lower!" . "\n");
 		
-		}elseif ($answer > $rightanswer) {
-			fwrite(STDOUT, "Guess lower!" . "\n");
-			$answer = trim(fgets(STDIN));
-		}else {
-			fwrite(STDOUT, "you're the best" . "\n");
-				exit(0);
-		}
+	
+	}
+	$answer = trim(fgets(STDIN));
+	$count++;
 }
+
+fwrite(STDOUT, "you're the best" . "\n");
+echo "It took you $count times" . "\n";
